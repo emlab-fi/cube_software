4d03171a7f434839799b4f5083339ace82fc4948
LoaDy588 <LoaDy588@seznam.cz>
2021-10-03 19:17:23 +0200

    start work on functional implementation

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ import serial.tools.list_ports as ports
 import serial
 
 class Application:
-    def __init__(self, measure_func = None):
+    def __init__(self, measure_func = None, init_func = None):
         window_base = partial(dpg.window, no_close=True, no_resize=True)
         self.__vp = dpg.create_viewport(title='Cube Control App', width=720, height=560, resizable=True, clear_color=((128, 128, 128, 255)))
         self.__automated_window = window_base(label="Automated measuring", width=720, height=280, collapsed=True, pos=(0, 0))
@@ -12,20 +12,45 @@ class Application:
         self.__connection_window = window_base(label="Connection", width=240, height=180, pos=(480, 20))
         self.__status_window = window_base(label="Status", width=240, height=180, pos=(480, 200))
         self.__control_window = window_base(label="Control", width=240, height=180, pos=(480, 380))
-        self.__error_window = dpg.window(no_close=True, no_collapse=True, label="Error", pos=(240, 240), modal=True, show=False, id="ERROR_WINDOW")
+        self.__error_window = dpg.window(no_close=True, no_collapse=True, label="Error", pos=(240, 240), modal=True, show=False, autosize=True, id="ERROR_WINDOW")
         self.__serial_port = None
         self.__measure_func = measure_func
+        self.__init_func = init_func
+        self.__sensor_initialized = False
         self.__cube = None
 
+
+    def __show_error(self, error_text):
+        dpg.set_value("ERROR_TEXT", error_text)
+        dpg.configure_item("ERROR_WINDOW", show=True)
+
+
+    def __log_info(self, text):
+        temp = dpg.get_value("CONSOLE_OUT")
+        temp += "[i] " + text + "\n"
+        dpg.set_value("CONSOLE_OUT", temp)
+
+
+    def __log_error(self, text):
+        temp = dpg.get_value("CONSOLE_OUT")
+        temp += "[e] " + text + "\n"
+        dpg.set_value("CONSOLE_OUT", temp)
+
+
     def __send_command(self):
+        if (self.__serial_port == None):
+            self.__show_error("Not connected!")
+            dpg.set_value("CONSOLE_IN", "")
+            return
         temp = dpg.get_value("CONSOLE_OUT")
         in_txt = dpg.get_value("CONSOLE_IN")
         if (in_txt == ""):
             return
-        temp += in_txt
+        temp += ">>> " + in_txt
         temp += "\n"
         dpg.set_value("CONSOLE_OUT", temp)
         dpg.set_value("CONSOLE_IN", "")
+
 
     def __update_final_pos(self):
         final_pos_x = dpg.get_value("AUTO_START_X") + dpg.get_value("AUTO_STEP_X") * dpg.get_value("AUTO_COUNT_X")
@@ -36,17 +61,21 @@ class Application:
         dpg.set_value("AUTO_FINAL_Z", final_pos_z)
         return
 
+
     def __set_current_pos(self):
         dpg.set_value("AUTO_START_X", dpg.get_value("CONTROL_X"))
         dpg.set_value("AUTO_START_Y", dpg.get_value("CONTROL_Y"))
         dpg.set_value("AUTO_START_Z", dpg.get_value("CONTROL_Z"))
 
+
     def __update_save_location(self, s, a, u):
         path = a["file_path_name"]
         dpg.set_value("AUTO_SAVE_FILE", path)
 
+
     def __start_measuring(self):
         print("AUTOMATED MEASURING NOT IMPLEMENTED")
+
 
     def __update_ports(self):
         port_list = ports.comports(include_links=True)
@@ -54,6 +83,7 @@ class Application:
         for port in port_list:
             names.append(port[0])
         dpg.configure_item("CONNECT_PORT", items=names)
+
 
     def __connect_serial(self):
         port = dpg.get_value("CONNECT_PORT")
@@ -67,6 +97,7 @@ class Application:
         dpg.set_value("CONSOLE_IN", "status")
         self.__send_command()
 
+
     def __disconnect_serial(self):
         if (self.__serial_port == None):
             self.__show_error("No port to close!")
@@ -75,19 +106,45 @@ class Application:
         self.__serial_port = None
         dpg.set_value("STATUS_CON", "Disconnected")
 
+
     def __goto_pos(self):
-        return
+        if (self.__serial_port == None):
+            self.__show_error("Not connected!")
+            return
+
+        self.__log_info("Moving...")
+        self.__log_error("Not implemented")
+
 
     def __measure(self):
-        if (self.__measure_func == None):
-            self.__show_error("No measurement function implemented!")
+        if (self.__serial_port == None):
+            self.__show_error("Not connected!")
+            return
+
+        if self.__measure_func == None or self.__init_func == None:
+            self.__show_error("No measure or init function!")
+
+        if not self.__sensor_initialized:
+            if not self.__init_func(self.__cube):
+                self.__log_error("sensor init failed")
+                return
+
+        error, value = self.__measure_func(self.__cube)
+        if (error != None):
+            self.__log_error(error)
+            return
+        self.__log_info(str(value))
+
 
     def __home(self):
+        if (self.__serial_port == None):
+            self.__show_error("Not connected!")
+            return
+
+        self.__log_info("Homing...")
+        self.__log_error("Not implemented")
         return
 
-    def __show_error(self, error_text):
-        dpg.set_value("ERROR_TEXT", error_text)
-        dpg.configure_item("ERROR_WINDOW", show=True)
 
     def __setup_automated(self):
         base_input_float = partial(dpg.add_input_float, default_value=0, step=1, step_fast=True, callback=self.__update_final_pos)
@@ -132,6 +189,7 @@ class Application:
                 dpg.add_text("Progress:")
                 dpg.add_text("0/0", id="AUTO_PROGRESS")
 
+
     def __setup_console(self):
         with self.__console_window:
             dpg.add_input_text(id="CONSOLE_OUT", width=460, height=480, multiline=True, readonly=True)
@@ -140,6 +198,7 @@ class Application:
             dpg.add_input_text(id="CONSOLE_IN", width=360, on_enter=True, callback=self.__send_command)
             dpg.add_same_line()
             dpg.add_button(label="Send", width=40, callback=self.__send_command)
+
 
     def __setup_connection(self):
         with self.__connection_window:
@@ -150,6 +209,7 @@ class Application:
             dpg.add_button(label="Connect", callback=self.__connect_serial)
             dpg.add_same_line()
             dpg.add_button(label="Disconnect", callback=self.__disconnect_serial)
+
 
     def __setup_status(self):
         with self.__status_window:
@@ -169,6 +229,7 @@ class Application:
             dpg.add_same_line()
             dpg.add_text("0", id="STATUS_ACK_ID")
 
+
     def __setup_control(self):
         base_input_float = partial(dpg.add_input_float, default_value=0, step=1, step_fast=True, min_value=-10000, max_value=10000)
         with self.__control_window:
@@ -185,10 +246,12 @@ class Application:
             dpg.add_button(label="Measure", callback=self.__measure)
             dpg.add_button(label="Home", callback=self.__home)
 
+
     def __setup_error(self):
         with self.__error_window:
             dpg.add_text("Error!", id="ERROR_TEXT")
             dpg.add_button(label="Ok", callback=lambda: dpg.configure_item("ERROR_WINDOW", show=False))
+
 
     def __setup_windows(self):
         self.__setup_automated()
@@ -198,15 +261,13 @@ class Application:
         self.__setup_control()
         self.__setup_error()
 
-    def run_app(self):
-        #with dpg.font_registry():
-        #    dpg.add_font("JetBrainsMono-Regular.ttf", 15, default_font=True)
 
+    def run_app(self):
         self.__setup_windows()
         dpg.setup_dearpygui(viewport=self.__vp)
         dpg.show_viewport(self.__vp)
         dpg.start_dearpygui()
 
 
-temp = Application()
-temp.run_app()
+if (__name__ == "__main__"):
+    Application().run_app()
