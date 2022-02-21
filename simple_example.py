d299a834ae88ba8dabe2da9dbb8414b31af768db
LoaDy588 <LoaDy588@seznam.cz>
2022-02-21 21:49:46 +0100

    change example.py

--- simple_example.py
+++ simple_example.py
@@ -1,7 +1,12 @@
 from pyCubeLib import CubeGUI
 from time import sleep
 
+
 def measure_func(cube):
+    """
+    Simple measure func for MLX90393.
+    Does no recalculation to uT.
+    """
     has_error, error, reply = cube.i2c_transfer(1, 1, 0x0C, [0x3F])
     if (has_error):
         return True, error, (0, 0, 0)
@@ -9,16 +14,21 @@ def measure_func(cube):
     has_error, error, reply = cube.i2c_transfer(9, 1, 0x0C, [0x4F])
     if (has_error):
         return True, error, (0, 0, 0)
-    
+
     payload_data = reply.get_payload()[1]
-    x = payload_data[3] + payload_data[4]
-    y = payload_data[5] + payload_data[6]
-    z = payload_data[7] + payload_data[8]
+    x = (payload_data[3] << 8) + payload_data[4]
+    y = (payload_data[5] << 8) + payload_data[6]
+    z = (payload_data[7] << 8) + payload_data[8]
 
     return False, None, (x, y, z)
 
+
 def init_func(cube):
+    """
+    Empty init.
+    """
     return True
 
+#run the GUI
 gui = CubeGUI(measure_func, init_func)
 gui.run_app()
